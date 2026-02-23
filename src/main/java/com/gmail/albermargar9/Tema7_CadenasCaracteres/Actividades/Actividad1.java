6e387f96e49a4d9808a925dcfaa024a0b6415b8d
Alber.mrtnn <albermargar9@gmail.com>
2026-02-23 12:36:23 +0100

    first commit

--- src/main/java/com/gmail/albermargar9/Tema7_CadenasCaracteres/Actividades/Actividad1.java
+++ src/main/java/com/gmail/albermargar9/Tema7_CadenasCaracteres/Actividades/Actividad1.java
@@ -1,4 +1,7 @@
 package com.gmail.albermargar9.Tema7_CadenasCaracteres.Actividades;
 
 public class Actividad1 {
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
 }
